8b1a2b0d6eaf29a072b138269ec5eb75f6b70398
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-20 20:46:04 -0300

    Update WerewolfEntity.java

--- src/main/java/moriyashiine/bewitchment/common/entity/living/WerewolfEntity.java
+++ src/main/java/moriyashiine/bewitchment/common/entity/living/WerewolfEntity.java
@@ -65,7 +65,7 @@ public class WerewolfEntity extends BWHostileEntity {
 				dropStack(getOffHandStack().split(1));
 			}
 			if (storedVillager != null && age % 20 == 0 && (getWorld().isDay() || BewitchmentAPI.getMoonPhase(getWorld()) != 0)) {
-				VillagerEntity entity | EntityVillagerMCA entity = EntityType.VILLAGER.create(getWorld());
+				EntityVillagerMCA entity = EntityType.VILLAGER.create(getWorld());
 				if (entity != null) {
 					PlayerLookup.tracking(this).forEach(trackingPlayer -> SpawnSmokeParticlesPacket.send(trackingPlayer, this));
 					getWorld().playSound(null, getX(), getY(), getZ(), BWSoundEvents.ENTITY_GENERIC_TRANSFORM, getSoundCategory(), getSoundVolume(), getSoundPitch());
