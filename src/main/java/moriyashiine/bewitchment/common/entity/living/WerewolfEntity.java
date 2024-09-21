265b7c329e64f68474ca81a2b0ba45e531e90d1d
Saint Saka <59484033+SaintSaka@users.noreply.github.com>
2024-09-21 16:02:46 -0300

    Update WerewolfEntity.java

--- src/main/java/moriyashiine/bewitchment/common/entity/living/WerewolfEntity.java
+++ src/main/java/moriyashiine/bewitchment/common/entity/living/WerewolfEntity.java
@@ -10,9 +10,9 @@ import moriyashiine.bewitchment.api.BewitchmentAPI;
 import moriyashiine.bewitchment.client.packet.SpawnSmokeParticlesPacket;
 import moriyashiine.bewitchment.common.entity.living.util.BWHostileEntity;
 import moriyashiine.bewitchment.common.misc.BWUtil;
-import moriyashiine.bewitchment.common.entity.MCA.EntityVillagerMCA;
 import moriyashiine.bewitchment.common.registry.BWComponents;
 import moriyashiine.bewitchment.common.registry.BWSoundEvents;
+import moriyashiine.bewitchment.common.entity.MCA.EntityVillagerMCA; // Imports MCA Villager
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
 import net.minecraft.entity.*;
@@ -23,6 +23,7 @@ import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.HostileEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.passive.MerchantEntity;
+import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
@@ -38,173 +39,135 @@ import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("ConstantConditions")
 public class WerewolfEntity extends BWHostileEntity {
-    public NbtCompound storedVillager;
+	public NbtCompound storedVillager;
 
-    public WerewolfEntity(EntityType<? extends HostileEntity> entityType, World world) {
-        super(entityType, world);
-    }
+	public WerewolfEntity(EntityType<? extends HostileEntity> entityType, World world) {
+		super(entityType, world);
+	}
 
-    public static DefaultAttributeContainer.Builder createAttributes() {
-        return MobEntity.createMobAttributes()
-            .add(EntityAttributes.GENERIC_MAX_HEALTH, 20)
-            .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 15)
-            .add(EntityAttributes.GENERIC_ARMOR, 20)
-            .add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 8)
-            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4)
-            .add(ReachEntityAttributes.ATTACK_RANGE, 1)
-            .add(StepHeightEntityAttributeMain.STEP_HEIGHT, 1);
-    }
+	public static DefaultAttributeContainer.Builder createAttributes() {
+		return MobEntity.createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, 20).add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 15).add(EntityAttributes.GENERIC_ARMOR, 20).add(EntityAttributes.GENERIC_ARMOR_TOUGHNESS, 8).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.4).add(ReachEntityAttributes.ATTACK_RANGE, 1).add(StepHeightEntityAttributeMain.STEP_HEIGHT, 1);
+	}
 
-    public static boolean canSpawn(EntityType<WerewolfEntity> type, ServerWorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
-        return world.toServerWorld().isNight() && BewitchmentAPI.getMoonPhase(world.toServerWorld().toServerWorld()) == 0 && 
-               HostileEntity.canSpawnInDark(type, world, spawnReason, pos, random);
-    }
+	public static boolean canSpawn(EntityType<WerewolfEntity> type, ServerWorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random) {
+		return world.toServerWorld().isNight() && BewitchmentAPI.getMoonPhase(world.toServerWorld().toServerWorld()) == 0 && HostileEntity.canSpawnInDark(type, world, spawnReason, pos, random);
+	}
 
-    @Override
-    public void tick() {
-        super.tick();
-        if (!getWorld().isClient) {
-            getArmorItems().forEach(stack -> dropStack(stack.split(1)));
-            if (BWUtil.isTool(getMainHandStack())) {
-                dropStack(getMainHandStack().split(1));
-            }
-            if (BWUtil.isTool(getOffHandStack())) {
-                dropStack(getOffHandStack().split(1));
-            }
-            
-            if (storedVillager != null && age % 20 == 0 && 
-                (getWorld().isDay() || BewitchmentAPI.getMoonPhase(getWorld()) != 0)) {
-                
-                VillagerEntity entity = EntityType.VILLAGER.create(getWorld());
-                if (entity != null) {
-                    PlayerLookup.tracking(this).forEach(trackingPlayer -> 
-                        SpawnSmokeParticlesPacket.send(trackingPlayer, this));
-                    
-                    getWorld().playSound(null, getX(), getY(), getZ(), 
-                        BWSoundEvents.ENTITY_GENERIC_TRANSFORM, getSoundCategory(), getSoundVolume(), getSoundPitch());
-                    
-                    entity.readNbt(storedVillager);
-                    entity.updatePositionAndAngles(getX(), getY(), getZ(), random.nextFloat() * 360, 0);
-                    entity.setHealth(entity.getMaxHealth() * (getHealth() / getMaxHealth()));
-                    entity.setFireTicks(getFireTicks());
-                    entity.clearStatusEffects();
-                    getStatusEffects().forEach(entity::addStatusEffect);
-                    BWComponents.CURSES_COMPONENT.maybeGet(entity).ifPresent(entityCursesComponent -> 
-                        BWComponents.CURSES_COMPONENT.maybeGet(this).ifPresent(thisCursesComponent -> {
-                            entityCursesComponent.getCurses().clear();
-                            thisCursesComponent.getCurses().forEach(entityCursesComponent::addCurse);
-                        }));
-                    
-                    BWComponents.WEREWOLF_VILLAGER_COMPONENT.get(entity).setStoredWerewolf(writeNbt(new NbtCompound()));
-                    getWorld().spawnEntity(entity);
-                    remove(RemovalReason.DISCARDED);
-                }
-            }
-        }
-    }
+	@Override
+	public void tick() {
+		super.tick();
+		if (!getWorld().isClient) {
+			getArmorItems().forEach(stack -> dropStack(stack.split(1)));
+			if (BWUtil.isTool(getMainHandStack())) {
+				dropStack(getMainHandStack().split(1));
+			}
+			if (BWUtil.isTool(getOffHandStack())) {
+				dropStack(getOffHandStack().split(1));
+			}
+			if (storedVillager != null && age % 20 == 0 && (getWorld().isDay() || BewitchmentAPI.getMoonPhase(getWorld()) != 0)) {
+				VillagerEntity entity = EntityType.VILLAGER.create(getWorld());
+				if (entity != null) {
+					PlayerLookup.tracking(this).forEach(trackingPlayer -> SpawnSmokeParticlesPacket.send(trackingPlayer, this));
+					getWorld().playSound(null, getX(), getY(), getZ(), BWSoundEvents.ENTITY_GENERIC_TRANSFORM, getSoundCategory(), getSoundVolume(), getSoundPitch());
+					entity.readNbt(storedVillager);
+					entity.updatePositionAndAngles(getX(), getY(), getZ(), random.nextFloat() * 360, 0);
+					entity.setHealth(entity.getMaxHealth() * (getHealth() / getMaxHealth()));
+					entity.setFireTicks(getFireTicks());
+					entity.clearStatusEffects();
+					getStatusEffects().forEach(entity::addStatusEffect);
+					BWComponents.CURSES_COMPONENT.maybeGet(entity).ifPresent(entityCursesComponent -> BWComponents.CURSES_COMPONENT.maybeGet(this).ifPresent(thisCursesComponent -> {
+						entityCursesComponent.getCurses().clear();
+						thisCursesComponent.getCurses().forEach(entityCursesComponent::addCurse);
+					}));
+					BWComponents.WEREWOLF_VILLAGER_COMPONENT.get(entity).setStoredWerewolf(writeNbt(new NbtCompound()));
+					getWorld().spawnEntity(entity);
+					remove(RemovalReason.DISCARDED);
+				}
+			}
+		}
+	}
 
-    public static WerewolfEntity transformVillagerToWerewolf(EntityVillagerMCA villager, World world) {
-        WerewolfEntity werewolf = new WerewolfEntity(EntityType.WEREWOLF, world);
-        
-        // Armazenar dados do villager
-        werewolf.storedVillager = villager.writeNbt(new NbtCompound());
-        
-        // Copiar atributos ou status do villager para o werewolf
-        werewolf.setHealth(villager.getHealth());
-        werewolf.setPosition(villager.getX(), villager.getY(), villager.getZ());
-        
-        return werewolf;
-    }
+	@Override
+	protected boolean hasShiny() {
+		return true;
+	}
 
-    // Método para verificar se deve transformar
-    private boolean shouldTransform() {
-        return getWorld().isNight();
-    }
+	@Override
+	public int getVariants() {
+		return getVariantsStatic();
+	}
 
-    @Override
-    protected boolean hasShiny() {
-        return true;
-    }
+	@Override
+	public EntityGroup getGroup() {
+		return BewitchmentAPI.DEMON;
+	}
 
-    @Override
-    public int getVariants() {
-        return getVariantsStatic();
-    }
+	@Nullable
+	@Override
+	protected SoundEvent getAmbientSound() {
+		return BWSoundEvents.ENTITY_WEREWOLF_AMBIENT;
+	}
 
-    @Override
-    public EntityGroup getGroup() {
-        return BewitchmentAPI.DEMON;
-    }
+	@Override
+	protected SoundEvent getHurtSound(DamageSource source) {
+		return BWSoundEvents.ENTITY_WEREWOLF_HURT;
+	}
 
-    @Nullable
-    @Override
-    protected SoundEvent getAmbientSound() {
-        return BWSoundEvents.ENTITY_WEREWOLF_AMBIENT;
-    }
+	@Override
+	protected SoundEvent getDeathSound() {
+		return BWSoundEvents.ENTITY_WEREWOLF_DEATH;
+	}
 
-    @Override
-    protected SoundEvent getHurtSound(DamageSource source) {
-        return BWSoundEvents.ENTITY_WEREWOLF_HURT;
-    }
+	@Override
+	public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityTag) {
+		EntityData data = super.initialize(world, difficulty, spawnReason, entityData, entityTag);
+		if (dataTracker.get(VARIANT) != 0) {
+			RegistryEntry<Biome> biome = world.getBiome(getBlockPos());
+			if (biome.isIn(ConventionalBiomeTags.FOREST)) {
+				dataTracker.set(VARIANT, random.nextBoolean() ? 1 : 2);
+			} else if (biome.isIn(ConventionalBiomeTags.TAIGA)) {
+				dataTracker.set(VARIANT, random.nextBoolean() ? 3 : 4);
+			}
+			if (biome.isIn(ConventionalBiomeTags.ICY)) {
+				dataTracker.set(VARIANT, random.nextBoolean() ? 5 : 6);
+			} else {
+				dataTracker.set(VARIANT, random.nextInt(getVariants() - 1) + 1);
+			}
+		}
+		if (spawnReason == SpawnReason.NATURAL || spawnReason == SpawnReason.SPAWN_EGG) {
+			storedVillager = EntityType.VILLAGER.create((World) world).writeNbt(new NbtCompound());
+		}
+		return data;
+	}
 
-    @Override
-    protected SoundEvent getDeathSound() {
-        return BWSoundEvents.ENTITY_WEREWOLF_DEATH;
-    }
+	@Override
+	public void readCustomDataFromNbt(NbtCompound nbt) {
+		super.readCustomDataFromNbt(nbt);
+		if (nbt.contains("StoredVillager")) {
+			storedVillager = nbt.getCompound("StoredVillager");
+		}
+	}
 
-    @Override
-    public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityTag) {
-        EntityData data = super.initialize(world, difficulty, spawnReason, entityData, entityTag);
-        if (dataTracker.get(VARIANT) != 0) {
-            RegistryEntry<Biome> biome = world.getBiome(getBlockPos());
-            if (biome.isIn(ConventionalBiomeTags.FOREST)) {
-                dataTracker.set(VARIANT, random.nextBoolean() ? 1 : 2);
-            } else if (biome.isIn(ConventionalBiomeTags.TAIGA)) {
-                dataTracker.set(VARIANT, random.nextBoolean() ? 3 : 4);
-            }
-            if (biome.isIn(ConventionalBiomeTags.ICY)) {
-                dataTracker.set(VARIANT, random.nextBoolean() ? 5 : 6);
-            } else {
-                dataTracker.set(VARIANT, random.nextInt(getVariants() - 1) + 1);
-            }
-        }
-        if (spawnReason == SpawnReason.NATURAL || spawnReason == SpawnReason.SPAWN_EGG) {
-            // Aqui você pode usar a EntityVillagerMCA
-            storedVillager = EntityType.VILLAGER.create((World) world).writeNbt(new NbtCompound());
-        }
-        return data;
-    }
+	@Override
+	public void writeCustomDataToNbt(NbtCompound nbt) {
+		super.writeCustomDataToNbt(nbt);
+		if (storedVillager != null) {
+			nbt.put("StoredVillager", storedVillager);
+		}
+	}
 
-    @Override
-    public void readCustomDataFromNbt(NbtCompound nbt) {
-        super.readCustomDataFromNbt(nbt);
-        if (nbt.contains("StoredVillager")) {
-            storedVillager = nbt.getCompound("StoredVillager");
-        }
-    }
+	@Override
+	protected void initGoals() {
+		goalSelector.add(0, new SwimGoal(this));
+		goalSelector.add(1, new MeleeAttackGoal(this, 1, true));
+		goalSelector.add(2, new WanderAroundFarGoal(this, 1));
+		goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 8));
+		goalSelector.add(3, new LookAroundGoal(this));
+		targetSelector.add(0, new RevengeGoal(this));
+		targetSelector.add(1, new ActiveTargetGoal<>(this, LivingEntity.class, 10, true, false, entity -> entity instanceof PlayerEntity || entity instanceof SheepEntity || entity instanceof MerchantEntity || entity.getGroup() == EntityGroup.ILLAGER));
+	}
 
-    @Override
-    public void writeCustomDataToNbt(NbtCompound nbt) {
-        super.writeCustomDataToNbt(nbt);
-        if (storedVillager != null) {
-            nbt.put("StoredVillager", storedVillager);
-        }
-    }
-
-    @Override
-    protected void initGoals() {
-        goalSelector.add(0, new SwimGoal(this));
-        goalSelector.add(1, new MeleeAttackGoal(this, 1, true));
-        goalSelector.add(2, new WanderAroundFarGoal(this, 1));
-        goalSelector.add(3, new LookAtEntityGoal(this, PlayerEntity.class, 8));
-        goalSelector.add(3, new LookAroundGoal(this));
-        targetSelector.add(0, new RevengeGoal(this));
-        targetSelector.add(1, new ActiveTargetGoal<>(this, LivingEntity.class, 10, true, false, 
-            entity -> entity instanceof PlayerEntity || entity instanceof SheepEntity || entity instanceof MerchantEntity || 
-            entity.getGroup() == EntityGroup.ILLAGER));
-    }
-
-    public static int getVariantsStatic() {
-        return 7;
-    }
+	public static int getVariantsStatic() {
+		return 7;
+	}
 }
